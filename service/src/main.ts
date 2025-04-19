121070bd6a589963df5169bf7655ec0f89085391
りおん@象徴-メッシ <h.rion.0910@gmail.com>
2025-04-19 14:47:09 +0900

    fix: CSRF

--- service/src/main.ts
+++ service/src/main.ts
@@ -1,14 +1,14 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: 'http://localhost:3000', // ← Next.js のURL
+      origin: ['https://employee-app-six-chi.vercel.app'],
       credentials: true,
     },
   });
-
-  await app.listen(4000); // ← ポートは4000にしてる前提
+  await app.listen(3000);
 }
 bootstrap();
