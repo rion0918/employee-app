e630a8c77900ddafc1b75ced3f2bbd5a4862c0c4
りおん@象徴-メッシ <h.rion.0910@gmail.com>
2025-04-19 14:36:51 +0900

    CSRFチェックを緩める

--- web/apollo-client.ts
+++ web/apollo-client.ts
@@ -1,8 +1,12 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
+// apollo-client.ts
 const httpLink = createHttpLink({
   uri: 'https://employee-app-service.onrender.com/graphql',
+  fetchOptions: {
+    mode: 'cors', // 明示的にCORS指定
+  },
 });
 
 const authLink = setContext((_, { headers }) => {
