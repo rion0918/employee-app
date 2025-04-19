60a15f59627377f2b0de696e54c6c81947eb48bf
りおん@象徴-メッシ <h.rion.0910@gmail.com>
2025-04-19 14:40:18 +0900

    fix: CSRF

--- web/apollo-client.ts
+++ web/apollo-client.ts
@@ -1,22 +1,20 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-// apollo-client.ts
 const httpLink = createHttpLink({
   uri: 'https://employee-app-service.onrender.com/graphql',
   fetchOptions: {
-    mode: 'cors', // 明示的にCORS指定
+    mode: 'cors',
   },
 });
 
-const authLink = setContext((_, { headers }) => {
-  return {
-    headers: {
-      ...headers,
-      'x-apollo-operation-name': 'default-operation',
-    },
-  };
-});
+// ✅ ここを追加・修正
+const authLink = setContext((_, { headers }) => ({
+  headers: {
+    ...headers,
+    'x-apollo-operation-name': 'default-op', // 空じゃなければOK
+  },
+}));
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
